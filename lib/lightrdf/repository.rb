69a59920c7161030d823abbfadfe99fd259797fc
Alberto Mardomingo <mardoalmar@gmail.com>
2011-01-12 14:06:28 +0100

    Changed the Sesame configuration options

--- lib/lightrdf/repository.rb
+++ lib/lightrdf/repository.rb
@@ -3,23 +3,20 @@ module RDF
     include MonitorMixin
     include Parser  
   
-    def initialize
+    def initialize(r_opt={})
       super()
-
-      r_opt = {}
-      # Looks for a configuration file in ~/.lightrdf
-      r_opt = YAML::load_file("#{ENV["HOME"]}/.scrappy/config.yml") if File.exist?("#{ENV["HOME"]}/.lightrdf/config.yml")
       
       # Assigns the default value to the nil options.
       @options = {"host"=>"http://localhost", "port"=>"8080", "repo"=>"men-ses", "time"=>"15", "format"=>"ntriples"}.merge(r_opt)
     end
     
     # Extracts the data in sesame from the indicated repositories
-    def get_data(contexts)
+    def get_data(contexts=["all"])
       synchronize do
       
         # Prepares the URL to request the data
-        dir = "#{@options["host"]}:#{@options["port"]}/openrdf-sesame/repositories/#{@options["repo"]}/statements?#{prepare_req_contexts(contexts)}"
+        context = contexts.map{|context| "context=#{context}"}*"&&"
+        dir = "#{@options["host"]}:#{@options["port"]}/openrdf-sesame/repositories/#{@options["repo"]}/statements?#{context}"
 
         # Asks for the data
         ntriples = RestClient.get dir, :content_type=>select_type
@@ -41,7 +38,7 @@ module RDF
     end
 
     # Adds data to sesame without removing the previous data
-    def post_data(graph, context)
+    def post_data(graph, context="")
       synchronize do
         # Prepares the dir to connect with the repository
         dir = "#{@options["host"]}:#{@options["port"]}/openrdf-sesame/repositories/#{@options["repo"]}/statements?context=%3C#{CGI::escape(context)}%3E"
@@ -54,32 +51,17 @@ module RDF
 
     protected
 
-    # Makes the contexts to add to the request
-    def prepare_req_contexts(contexts)
-      result = ""
-      if contexts[0] != nil
-        i = 0
-        while i < contexts.length
-          result += "context=#{contexts[i]}"
-          i += 1
-          result += "&&" if i != contexts.length
-        end
-      end
-      return result
-    end
-
     # Selects the type of data to send to sesame
     def select_type
-      type = case
-        when @options["format"] == "rdfxml" then 'application/rdf+xml'
-        when @options["format"] == "ntriples" then 'text/plain'
-        when @options["format"] == "turtle" then 'application/x-turtle'
-        when @options["format"] == "n3" then 'text/rdf+n3'
-        when @options["format"] == "trix" then 'application/trix'
-        when @options["format"] == "trig" then 'application/x-trig'
+      case @options["format"]
+        when "rdfxml" then 'application/rdf+xml'
+        when "ntriples" then 'text/plain'
+        when "turtle" then 'application/x-turtle'
+        when "n3" then 'text/rdf+n3'
+        when "trix" then 'application/trix'
+        when "trig" then 'application/x-trig'
         else 'Unknown'
       end
-      return type
     end
   end
 end
